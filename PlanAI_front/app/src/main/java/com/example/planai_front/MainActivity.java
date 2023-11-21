e34e37efa60726db12575a052c2430a0d99efa3c
legolas089 <legolasism777@gmail.com>
2023-11-22 02:23:25 +0900

    Fix: date pick action

--- PlanAI_front/app/src/main/java/com/example/planai_front/MainActivity.java
+++ PlanAI_front/app/src/main/java/com/example/planai_front/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         calendarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
+                Intent intent = new Intent(MainActivity.this, MaterialCalendarActivity.class);
                 startActivity(intent);
             }
         });
