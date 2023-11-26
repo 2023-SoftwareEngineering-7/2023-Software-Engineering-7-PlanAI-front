a7c606ae07d41caa6d428dcbfd72ad34d17ef91c
legolas089 <legolasism777@gmail.com>
2023-11-27 00:40:43 +0900

    feat: bottom menu bars

--- PlanAI_front/app/src/main/java/com/example/planai_front/FriendlistActivity.java
+++ PlanAI_front/app/src/main/java/com/example/planai_front/FriendlistActivity.java
@@ -13,6 +13,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import java.util.ArrayList;
 
 
@@ -31,6 +33,42 @@ public class FriendlistActivity extends AppCompatActivity {
         Button.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), AddfriendActivity.class);
             startActivity(intent);
+        });
+
+        setupBottomNavigationBar();
+    }
+
+    private void setupBottomNavigationBar() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomnavigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.navigation_calendar) {
+                startActivity(new Intent(FriendlistActivity.this, MaterialCalendarActivity.class));
+
+            }else if (item.getItemId() == R.id.navigation_friend) {
+                startActivity(new Intent(FriendlistActivity.this, FriendlistActivity.class));
+                // 추가 버튼에 대한 처리를 여기에 작성합니다.
+                // 예: case R.id.navigation_new_button:
+                // 버튼에 대한 액션 구현
+
+            } else if (item.getItemId() == R.id.navigation_home){
+                startActivity(new Intent(FriendlistActivity.this, MaterialCalendarActivity.class));
+                // 추가 버튼에 대한 처리를 여기에 작성합니다.
+                // 예: case R.id.navigation_new_button:
+                // 버튼에 대한 액션 구현
+
+            }else if (item.getItemId() == R.id.navigation_post){
+                startActivity(new Intent(FriendlistActivity.this, BoardActivity.class));
+                // 추가 버튼에 대한 처리를 여기에 작성합니다.
+                // 예: case R.id.navigation_new_button:
+                // 버튼에 대한 액션 구현
+
+            }else if (item.getItemId() == R.id.navigation_setting) {
+                startActivity(new Intent(FriendlistActivity.this, MaterialCalendarActivity.class));
+                // 추가 버튼에 대한 처리를 여기에 작성합니다.
+                // 예: case R.id.navigation_new_button:
+                // 버튼에 대한 액션 구현
+            }
+            return true;
         });
     }
 
--- PlanAI_front/app/src/main/java/com/example/planai_front/MaterialCalendarActivity.java
+++ PlanAI_front/app/src/main/java/com/example/planai_front/MaterialCalendarActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import com.example.planai_front.create.ShowDayActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
@@ -17,25 +18,62 @@ Intented from bottomNavigation - id navigation_calendar
 2) date selection available
 3) bottom navigation bar available
  */
-public class MaterialCalendarActivity extends AppCompatActivity{
+public class MaterialCalendarActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.materialcalendar_main);
 
-        MaterialCalendarView materialCalendarView = (MaterialCalendarView) findViewById(R.id.calendarView);
-        materialCalendarView.setOnDateChangedListener((new OnDateSelectedListener() {
+        setupCalendarView();
+        setupBottomNavigationBar();
+    }
+
+    private void setupCalendarView() {
+        MaterialCalendarView materialCalendarView = findViewById(R.id.calendarView);
+        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
             @Override
-            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
-                //Toast.makeText(MaterialCalendarActivity.this, ""+date,Toast.LENGTH_SHORT).show();
-
+            public void onDateSelected(@NonNull MaterialCalendarView widget,
+                                       @NonNull CalendarDay date,
+                                       boolean selected) {
                 Intent intent = new Intent(MaterialCalendarActivity.this, ShowDayActivity.class);
-                intent.putExtra("date",date.getDate().toString());
-                intent.putExtra("fullDateInfo",date.getDate().getDayOfWeek().toString());
+                intent.putExtra("date", date.getDate().toString());
+                intent.putExtra("fullDateInfo", date.getDate().getDayOfWeek().toString());
                 startActivity(intent);
-
             }
-        }));
+        });
+    }
 
+    private void setupBottomNavigationBar() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomnavigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.navigation_calendar) {
+                startActivity(new Intent(MaterialCalendarActivity.this, MaterialCalendarActivity.class));
+
+            }else if (item.getItemId() == R.id.navigation_friend) {
+                startActivity(new Intent(MaterialCalendarActivity.this, FriendlistActivity.class));
+                // 추가 버튼에 대한 처리를 여기에 작성합니다.
+                // 예: case R.id.navigation_new_button:
+                // 버튼에 대한 액션 구현
+
+            } else if (item.getItemId() == R.id.navigation_home){
+                startActivity(new Intent(MaterialCalendarActivity.this, MaterialCalendarActivity.class));
+                // 추가 버튼에 대한 처리를 여기에 작성합니다.
+                // 예: case R.id.navigation_new_button:
+                // 버튼에 대한 액션 구현
+
+            }else if (item.getItemId() == R.id.navigation_post){
+                startActivity(new Intent(MaterialCalendarActivity.this, BoardActivity.class));
+                // 추가 버튼에 대한 처리를 여기에 작성합니다.
+                // 예: case R.id.navigation_new_button:
+                // 버튼에 대한 액션 구현
+
+            }else if (item.getItemId() == R.id.navigation_setting) {
+                startActivity(new Intent(MaterialCalendarActivity.this, MaterialCalendarActivity.class));
+                // 추가 버튼에 대한 처리를 여기에 작성합니다.
+                // 예: case R.id.navigation_new_button:
+                // 버튼에 대한 액션 구현
+            }
+            return true;
+        });
     }
 }
