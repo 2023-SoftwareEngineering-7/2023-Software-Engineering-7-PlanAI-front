9287f55a2277e33fe4bc061e0a44cb7e02fc70a2
legolas089 <legolasism777@gmail.com>
2023-11-28 12:10:10 +0900

    Fix: UserId Long type
    UserId가 1111L이라면, endpoint에는 L빼고 1111만 넣어줘야 함.
    코드(|ApiService.java): @(POST)(user/1111/Schedule)
    DB: /user/{userId}/Schedule

--- PlanAI_front/app/src/main/java/com/example/planai_front/Server/ApiService.java
+++ PlanAI_front/app/src/main/java/com/example/planai_front/Server/ApiService.java
@@ -8,6 +8,6 @@ import retrofit2.http.POST;
 
 public interface ApiService {
     //TODO:서버에서 정의된 엔드포인트?
-    @POST("user/{userId}/Schedule") // 여기서 "schedule/create"는 서버에서 정의된 엔드포인트를 나타냅니다.
+    @POST("user/1111/Schedule") // 여기서 "schedule/create"는 서버에서 정의된 엔드포인트를 나타냅니다.
     Call<Server_ScheduleDTO> createSchedule(@Body Server_ScheduleDTO schedule);
 }
