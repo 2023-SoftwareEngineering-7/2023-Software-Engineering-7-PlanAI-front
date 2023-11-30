cd188f5b0b82ec719c35154853a5f31c8b279085
legolas089 <legolasism777@gmail.com>
2023-11-30 19:15:16 +0900

    initial commit of ChatGPT

--- PlanAI_front/app/src/main/java/com/example/planai_front/create/CreateTaskActivity.java
+++ PlanAI_front/app/src/main/java/com/example/planai_front/create/CreateTaskActivity.java
@@ -23,6 +23,7 @@ import java.util.Locale;
 //전달받은 todayDate(2023-11-22 형식을 가짐)을 id로 해서 서버에 Task 등록
 //popup_layout을 띄움(아래에서 올라오는 pop-up 또는 popup 처럼 생긴 새로운 xml 페이지. 편한걸로 할것!!!)
 //뒤로가기 버튼을 통해 ShowDayActivity로 돌아감
+//ChatGPT 더하기
 public class CreateTaskActivity extends AppCompatActivity {
 
     private String todayDate;
