d57cefec06fc50f539bc8cc64e36f2aa3e28036c
woohyunbin <yuxuanbin@naver.com>
2023-12-21 18:53:23 +0900

    gogo

--- PlanAI_front/app/src/main/java/com/example/planai_front/AddfriendActivity.java
+++ PlanAI_front/app/src/main/java/com/example/planai_front/AddfriendActivity.java
@@ -2,9 +2,17 @@ package com.example.planai_front;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class AddfriendActivity extends AppCompatActivity {
     private Button button4;
@@ -13,6 +21,28 @@ public class AddfriendActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addfriend);
+
+        EditText editText_find = findViewById(R.id.editText_find);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        // 인플레이트할 레이아웃 가져오기
+        View addfriend_item = inflater.inflate(R.layout.addfriend_item, null);
+        View friendlist = inflater.inflate(R.layout.activity_friend_list, null);
+
+        TextView FriendName = addfriend_item.findViewById(R.id.name_item);
+
+
+        Button Button1 = (Button) findViewById(R.id.addfriend_button);
+        Button1.setOnClickListener(view -> {
+
+            String name = editText_find.getText().toString();
+            FriendName.setText(name);
+            Log.d("name", "Title: " + FriendName);
+        // 부모 레이아웃 가져오기
+            LinearLayout showfriend = findViewById(R.id.showfriend);
+        // 커스텀 레이아웃을 부모 레이아웃에 추가
+            showfriend.addView(addfriend_item);
+        });
+
 
         Button Button = (Button) findViewById(R.id.buttonx);
         Button.setOnClickListener(view -> {
--- PlanAI_front/app/src/main/java/com/example/planai_front/MainActivity.java
+++ PlanAI_front/app/src/main/java/com/example/planai_front/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.planai_front;
-//branch for Calendal - calendar2: 23.11.25 created
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -15,39 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView calendarButton = findViewById(R.id.testCalendar);
+        Button calendarButton = findViewById(R.id.testCalendar);
         calendarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MaterialCalendarActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-        Button loginButton = findViewById(R.id.login);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.out.println("로그인 버튼 터치됨");
-                Intent intent = new Intent(MainActivity.this, GoogleSignInActivity.class);
+                Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
                 startActivity(intent);
             }
         });
     }
 }
-/*
-
-    <Button
-        android:id="@+id/testCalendar"
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:layout_marginTop="45dp"
-        android:layout_marginBottom="221dp"
-        android:text="TestCalendar"
-        app:layout_constraintBottom_toBottomOf="parent"
-        app:layout_constraintEnd_toEndOf="parent"
-        app:layout_constraintStart_toStartOf="parent"
-        app:layout_constraintTop_toBottomOf="@+id/textView" />
-
- */
